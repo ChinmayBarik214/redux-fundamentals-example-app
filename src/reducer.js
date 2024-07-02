dbd674dc95a4725fa0a051f3aad6c51378978974
Chinmay <chinmaybarik214@gmail.com>
2024-07-02 11:46:20 +0530

    3 reducers written before split

--- src/reducer.js
+++ src/reducer.js
@@ -10,14 +10,56 @@ const initialState = {
   },
 }
 
+function nextTodoId(todos) {
+  const maxId = todos.reduce((maxId, todo) => Math.max(todo.id, maxId), -1)
+  return maxId + 1
+} 
+
 // Use the initialState as a default value
 export default function appReducer(state = initialState, action) {
-  // The reducer normally looks at the action type field to decide what happens
   switch (action.type) {
-    // Do something here based on the different types of actions
+    case 'todos/todoAdded': {
+      return {
+        ...state,
+        todos: [
+          ...state.todos,
+          {
+            id: nextTodoId(state.todos),
+            text: action.payload,
+            completed: false
+          }
+        ]
+      }
+    }
+    case 'todos/todoToggled': {
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (todo.id !== action.payload) {
+            return todo
+          }
+
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        })
+      }
+    }
+    case 'filters/statusFilterChanged': {
+      return {
+        // Copy the whole state
+        ...state,
+        // Overwrite the filters value
+        filters: {
+          // copy the other filter fields
+          ...state.filters,
+          // And replace the status field with the new value
+          status: action.payload
+        }
+      }
+    }
     default:
-      // If this reducer doesn't recognize the action type, or doesn't
-      // care about this specific action, return the existing state unchanged
       return state
   }
 }
